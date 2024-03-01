30868e7952e3eca9e741ac3597ec98e7c36fdf0e
Lò Văn Anh <161822153+Lovananh@users.noreply.github.com>
2024-03-01 21:35:29 +0700

    Update app.js

--- app.js
+++ app.js
@@ -25,37 +25,53 @@ const html = `
         });
       }, 500);
     </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
+    <style >
+            body{
+                text-align: center;
+            }
+            .box{
+                line-height: 50px;
+                height: 50px;
+                width: 190px;
+                background-color:rgb(99, 67, 180);
+                color: white;
+            }
+            .color-text{
+                    color :rgb(32, 186, 228);
+            }
+            .co-chu{
+                font-family:  Arial, sans-serif; 
+            }
+            .colored-botton{
+                border: dark 2px ridge;
+                border-radius: 10px;
+                line-height: 50px;
+                height: 50px;
+                width: 190px;
+                background-color:black;
+                color: white;
+        }
+        .colored-botton:hover{
+            background-color: yellow ;
+            transition: 0.4s background-color;
+        }
+        </style>
+    </head>
+
+    <body>
+        <center>
+            <p class="box">190 x 50</p> 
+        </center>
+        <h1>
+             <p class="co-chu">Best <span class="color-text">Share Hosting</span> Company </p> 
+            </h1>
+        <h4> 
+            <p class="co-chu">With this responsive landing page template, you can promote your all hosting, domain and email servers. </p>
+         </h4>
+        <p>
+            <button type="submit" class="colored-botton">View Plan </button>
+            <button type="submit" class="colored-botton">All Features </button>
+        </p>
+    </body>
 </html>
 `
